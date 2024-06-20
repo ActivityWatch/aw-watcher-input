6a0175d4c957ae268de9f8374510eb1f2b5a0e21
Joeyrsp <joeysuttonpreece@gmail.com>
2024-06-20 20:29:57 +1000

    feat: add toml config and arg parsing for poll time

--- src/aw_watcher_input/__main__.py
+++ src/aw_watcher_input/__main__.py
@@ -1,4 +1,11 @@
-from aw_watcher_input.main import main
+from aw_watcher_input.main import main as _main
+from aw_watcher_input.config import parse_args
+
+def main() -> None:
+    args = parse_args()
+
+    # Start watcher
+    _main(args, testing=args.testing)
 
 if __name__ == "__main__":
     main()
